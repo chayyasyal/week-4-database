c89e2d4ca17ee240f99206627d7a4541f549c68d
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-17 10:47:57 +0100

    User stories from task 1 of this week's challenge

---
+++ userstories.rb
@@ -0,0 +1,51 @@
+#User stories
+##  Write User stories/features for showing a list of links
+```
+ As a user I want to be able to recall previously stored links
+ So that I car read them on my own time
+ I want a web app to display them neatly
+```
+##  Write User stories/features for adding new links
+
+```
+As a user I want to be able to store new links
+So that I will not loose them
+I want to add new links to my list
+```
+
+##  Write User stories/features for adding tags to links
+```
+As a bookmark app user
+So that I can find my links quickly
+I want to add tags to my links on the list
+```
+## Write User stories/features for filtering links by a tag
+```
+As a bookmark app user
+So that I can find my links quickly
+I want to search my  stored links by tags (keywords)
+```
+
+# Walkthrough stories 
+
+```
+
+As a time-pressed user
+So that I can quickly go to web sites I regularly visit
+I would like to see a list of links on the homepage
+
+As a time-pressed user
+So that I can quickly find web sites I recently bookmarked
+I would like to see links in descending chronological order
+
+As a time-pressed user
+So that I can save a website
+I would like to add the site's address and title to my bookmark manager
+
+As a time-pressed user
+So that I can organise my many links into different categories for ease of search
+I would like to add tags to the links in my bookmark manager
+
+As a time-pressed user
+So that I can quickly find links on a particular topic
+I would like to filter links by tag
